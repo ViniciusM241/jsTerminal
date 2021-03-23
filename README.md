e224fc1ab97d612a824ee75ac7c619f22c158e94
ViniciusM241 <viniciusmelo241@gmail.com>
2021-03-23 09:25:52 -0300

    JsTerminal v0.1

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 > O seu terminal dentro de uma página html.
 ---
 
+## Quais funções o JsTerminal possui?
+Há vários programas já embutidos na primeira versão do JsTerminal (versão 0.1), como:
+
+> Gerenciamento de usuários:
+- su
+- sudo (su)
+- passwd
+- adduser (somente como usuário root)
+- whoami
+
+> Outros comandos do terminal:
+- echo
+- clear
+
+
+
 ## Como implementar?
 
 São necessários alguns passos para conseguir utilizá-lo.
@@ -9,12 +25,19 @@ São necessários alguns passos para conseguir utilizá-lo.
 - [ ] Adicionar o JS.
 - [ ] 
 - [ ] 
-### Adicionando ao HTML:
+
+### ***Adicionando ao HTML***:
 Para poder utilizar o terminal em uma página HTML, primeiro precisamos adicionar o seguinte textarea no fonte:
 ~~~~html 
 <textarea id="terminal" type="text" cols="70" rows="20" spellcheck="false" wrap="off"></textarea>
 ~~~~
 O terminal será renderizado direto dentro dessa tag.
 
-## Adicionando o JavaScript:
 
+### ***Adicionando o JavaScript***:
+Para adicionar as funcionalidades do terminal. é necessário, também, incrementar o arquivo ```js-terminal.js``` ao seu HTML.
+~~~~html 
+<script src="js-terminal.js" type="text/javascript"></script>
+~~~~
+
+Após os dois procedimentos, o seu JsTerminal está pronto para ser usado em sua página.
--- index.js
+++ index.js
@@ -1,120 +1,108 @@
-/*
-Posicionar Cursor em Input
-const length = terminal.value.length;
-terminal.setSelectionRange(length -1, length);
-*/
+const initTerminal = () => {
 
+    const terminal = document.querySelector("#terminal");
+    let lastCommands = [];
+    let auxLastCommands = [];
+    let index = -1;
 
-const terminal = document.querySelector("#terminal");
-let lastCommands = [];
-let auxLastCommands = [];
-let index = -1;
-
-// BIBLIOTECA DE PROGRAMAS PARA O TERMINAL
-const library = {
-    users: [
-        {name: 'root', pwd: '02', ip: '127.0.0.1'},
-        {name: 'vinicius', pwd: '01', ip: '127.0.0.1'}
-    ],
-    loggedUser: 1,
-    clear: function(){
-        this.setTerminal('', true);
-    },
-    whoami: function() {
-        this.setTerminal(this.users[this.loggedUser]['name']);
-    },
-    sudo: function(params) {
-        console.log(params)
-        if (params[0] === 'su')
-            this.su(["root"]);
-        else
-            this[params[0]](params.slice(params[0]))
-    },
-    su: async function(params) {
-        console.log(params.length)
-        console.log(params)
-        if (params.length != 1)
-            this.setTerminal('terminal: syntax incorrect.');
-        else {
-            this.users.forEach((x, i) => {
-                if (x['name'] === params[0])
-                    index = i;
-            });
-            if (index === -1)
-                this.setTerminal(`terminal: ${params[0]} user not found.`);
-            else {
-                await this.getTerminal(`Password: `);
-                if (response === this.users[index]['pwd']) {
-                    this.loggedUser = index;
-                    this.setTerminal('');
-                    index = -1;
-                } else
-                    this.setTerminal('terminal: incorrect password.');
-            }
-        }
-    },
-    adduser: async function() {
-        if (this.loggedUser !== 0)
-            this.setTerminal('terminal: Just root user can add user.');
-        else {
-            await this.getTerminal('Name: ');
-            let user = {name: response, pwd: '', ip: '127.0.0.1'};
-            if (response === '')
-                this.setTerminal('terminal: Name can not be blank.')
+    // BIBLIOTECA DE PROGRAMAS PARA O TERMINAL
+    const library = {
+        users: [
+            {name: 'root', pwd: '02', ip: '127.0.0.1'},
+            {name: 'vinicius', pwd: '01', ip: '127.0.0.1'}
+        ],
+        loggedUser: 1,
+        clear: function(){
+            setTerminal('', true);
+        },
+        whoami: function() {
+            setTerminal(this.users[this.loggedUser]['name']);
+        },
+        sudo: function(params) {
+            if (params[0] === 'su')
+                this.su(["root"]);
+            else
+                this[params[0]](params.slice(params[0]))
+        },
+        su: async function(params) {
+            if (params.length != 1)
+                setTerminal('terminal: syntax incorrect.');
             else {
                 this.users.forEach((x, i) => {
-                    if (x['name'] === response)
-                        index = i;
+                    if (x['name'] === params[0])
+                    index = i;
                 });
-                if (index !== -1)
-                    this.setTerminal("terminal: This user already exists.");
+                if (index === -1)
+                    setTerminal(`terminal: ${params[0]} user not found.`);
                 else {
-                    await this.getTerminal('Password: ');
-                    if (response !== '') {
-                        user['pwd'] = response;
-                        console.log(user);
-                        this.users.push(user)
-                        console.log(this.users)
-                    this.setTerminal('');
-                    } else {
-                        this.setTerminal('terminal: Password can not be blank.')
+                    await getTerminal(`Password: `);
+                    if (response === this.users[index]['pwd']) {
+                        this.loggedUser = index;
+                        setTerminal('');
+                        index = -1;
+                    } else
+                        setTerminal('terminal: incorrect password.');
+                }
+            }
+        },
+        adduser: async function() {
+            if (this.loggedUser !== 0)
+                setTerminal('terminal: Just root user can add user.');
+            else {
+                await getTerminal('Name: ');
+                let user = {name: response, pwd: '', ip: '127.0.0.1'};
+                if (response === '')
+                    setTerminal('terminal: Name can not be blank.')
+                else {
+                    this.users.forEach((x, i) => {
+                        if (x['name'] === response)
+                        index = i;
+                    });
+                    if (index !== -1)
+                    setTerminal("terminal: This user already exists.");
+                    else {
+                        await getTerminal('Password: ');
+                        if (response !== '') {
+                            user['pwd'] = response;
+                            this.users.push(user)
+                            setTerminal('');
+                        } else {
+                            setTerminal('terminal: Password can not be blank.')
+                        }
                     }
                 }
             }
-        }
-    },
-    passwd: async function() {
-        await this.getTerminal(`Changing password of ${this.users[this.loggedUser]['name']}\nCurrent password: `);
-        if (response === this.users[this.loggedUser]['pwd']) {
-            await this.getTerminal('New password: ');
-            if (response !== '' && response !== this.users[this.loggedUser]['pwd'])
+        },
+        passwd: async function() {
+            await getTerminal(`Changing password of ${this.users[this.loggedUser]['name']}\nCurrent password: `);
+            if (response === this.users[this.loggedUser]['pwd']) {
+                await getTerminal('New password: ');
+                if (response !== '' && response !== this.users[this.loggedUser]['pwd'])
                 this.users[this.loggedUser]['pwd'] = response;
-                this.setTerminal('');
-        } else
-            this.setTerminal('terminal: incorrect password.');
-    },
-    echo: function(params) {
-        const s = params.join(' ').replace('"', "").replace('"', "");
-        this.setTerminal(s);
-    },
-    bc: function(params) {
-        console.log(params)
-        this.setTerminal('');
-    },
-    matriz: async function(params) {
-        console.log("Oi");
-        this.setTerminal('');
-    },
-    setTerminal: function(response, clean) {
-        const terminalTextDefault = `${this.users[this.loggedUser]['name']}@${this.users[this.loggedUser]['ip']}>>`;
+                setTerminal('');
+            } else
+                setTerminal('terminal: incorrect password.');
+        },
+        echo: function(params) {
+            const s = params.join(' ').replace('"', "").replace('"', "");
+            setTerminal(s);
+        },
+        matriz: async function(params) {
+            
+        }
+    }
+
+    const setTerminal = (response, clean) => {
+        const terminalTextDefault = `${library.users[library.loggedUser]['name']}@${library.users[library.loggedUser]['ip']}>>`;
         if (clean)
-                terminal.value = `${terminalTextDefault}`;
+        terminal.value = `${terminalTextDefault}`;
         else if (response === '')
-            terminal.value += `\n${terminalTextDefault}`;
+        terminal.value += `\n${terminalTextDefault}`;
         else
-            terminal.value += `\n${response}\n${terminalTextDefault}`;
-    },
-    getTerminal: async function(ask) {
+        terminal.value += `\n${response}\n${terminalTextDefault}`;
+    }
+
+    const getTerminal = async (ask) => {
         terminal.value += `\n${ask}`;
         answer = terminal.value;
         let response;
@@ -122,123 +110,119 @@ const library = {
         terminal.addEventListener('keydown', listenTerminalMain);
         return response;
     }
-}
 
 
-// FUNCIONALIDADES BÁSICAS PARA O FUNCIONAMENTO DO TERMINAL
-const listenTerminalAnswer = (answer) => {
-    return new Promise(resolve => {
-        terminal.removeEventListener('keydown', listenTerminalMain);
-        terminal.addEventListener('keydown', function get(e) {
-            if (e.key === 'Enter') {
-                response = terminal.value.slice(answer.length - 1).trim();
-                terminal.removeEventListener('keydown', get);
-                resolve(response);
-            }
-        });
-    })
-}
+    // FUNCIONALIDADES BÁSICAS PARA O FUNCIONAMENTO DO TERMINAL
+    const listenTerminalAnswer = (answer) => {
+        return new Promise(resolve => {
+            terminal.removeEventListener('keydown', listenTerminalMain);
+            terminal.addEventListener('keydown', function get(e) {
+                if (e.key === 'Enter') {
+                    response = terminal.value.slice(answer.length - 1).trim();
+                    terminal.removeEventListener('keydown', get);
+                    resolve(response);
+                }
+            });
+        })
+    }
 
-const listenTerminalAvoidBackspace = (e) => {
-    let terminalTextLast = terminal.value;
-    if (e.key === 'Backspace')
+    const listenTerminalAvoidBackspace = (e) => {
+        let terminalTextLast = terminal.value;
+        if (e.key === 'Backspace')
         if (terminalTextLast[terminalTextLast.length - 1] == ">" || terminalTextLast[terminalTextLast.length - 1] == ":")
-            terminal.value = terminalTextLast + '>';
-}
+        terminal.value = terminalTextLast + '>';
+    }
 
-const listenTerminalAvoidChangeCursor = () => {
-    const length = terminal.value.length;
-    terminal.setSelectionRange(length, length);
-}
+    const listenTerminalAvoidChangeCursor = () => {
+        const length = terminal.value.length;
+        terminal.setSelectionRange(length, length);
+    }
 
-const listenTerminalAvoidMoveCursor = (e) => {
-    if (e.key === 'ArrowLeft'){
-        const a = [...terminal.value]
-        a.forEach((char, i) => {
-            if (char === '>')
+    const listenTerminalAvoidMoveCursor = (e) => {
+        if (e.key === 'ArrowLeft'){
+            const a = [...terminal.value]
+            a.forEach((char, i) => {
+                if (char === '>')
                 index = i + 1;
-        })
-        console.log(index);
-        console.log(terminal.value.length);
-        if (terminal.value.length === index){
-            const length = terminal.value.length;
-            terminal.setSelectionRange(length + 1, length);
+            })
+            if (terminal.value.length === index){
+                const length = terminal.value.length;
+                terminal.setSelectionRange(length + 1, length);
+            }
         }
     }
-}
 
-const listenTerminalMain = (e) => {
-    if (e.key === 'ArrowUp'){
-        const lastCommand = lastCommands.pop();
-        let index;
-
-        if (lastCommand === undefined)
+    const listenTerminalMain = (e) => {
+        if (e.key === 'ArrowUp'){
+            const lastCommand = lastCommands.pop();
+            let index;
+            
+            if (lastCommand === undefined)
             return;
-        auxLastCommands.push(lastCommand);
-        const a = [...terminal.value]
-        a.forEach((char, i) => {
-            if (char === '>')
+            auxLastCommands.push(lastCommand);
+            const a = [...terminal.value]
+            a.forEach((char, i) => {
+                if (char === '>')
                 index = i + 1;
-        })
-        terminal.value = terminal.value.slice(0, index) + lastCommand;
-    }
-    if (e.key === 'ArrowDown'){
-        let lastCommand = auxLastCommands.pop();
-        let index;
-
-        if (lastCommand === undefined)
+            })
+            terminal.value = terminal.value.slice(0, index) + lastCommand;
+        }
+        if (e.key === 'ArrowDown'){
+            let lastCommand = auxLastCommands.pop();
+            let index;
+            
+            if (lastCommand === undefined)
             lastCommand = '';
-        else {
-            lastCommands.push(lastCommand);
+            else {
+                lastCommands.push(lastCommand);
+            }
+            const a = [...terminal.value]
+            a.forEach((char, i) => {
+                if (char === '>')
+                index = i + 1;
+            })
+            terminal.value = terminal.value.slice(0, index) + lastCommand;
         }
-        const a = [...terminal.value]
+        if (e.key == 'Enter')
+        captureFunction(terminal.value);
+    }
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let terminalTextInit =`JS Terminal v0.1\nby Vinicius Melo, GitHub User: ViniciusM241\n\n${library['users'][library.loggedUser]['name']}@${library['users'][library.loggedUser]['ip']}>>`;
+        terminal.value = terminalTextInit;
+        terminal.focus();
+        terminal.addEventListener('keydown', listenTerminalMain);
+        terminal.addEventListener('keydown', listenTerminalAvoidBackspace);
+        terminal.addEventListener('click', listenTerminalAvoidChangeCursor);
+        terminal.addEventListener('keydown', listenTerminalAvoidMoveCursor)
+    })
+
+    const captureFunction = (inputText) => {
+        const a =  [...inputText];
+        let stringUser = "";
+        let index, program, params;
+
         a.forEach((char, i) => {
             if (char === '>')
-                index = i + 1;
-        })
-        terminal.value = terminal.value.slice(0, index) + lastCommand;
-    }
-    if (e.key == 'Enter')
-        captureFunction(terminal.value);
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    let terminalTextInit = 
-    `JS Terminal v0.1
-by Vinicius Melo, GitHub User: ViniciusM241
-
-${library['users'][library.loggedUser]['name']}@${library['users'][library.loggedUser]['ip']}>>`;
-    terminal.value = terminalTextInit;
-    terminal.focus();
-    terminal.addEventListener('keydown', listenTerminalMain);
-    terminal.addEventListener('keydown', listenTerminalAvoidBackspace);
-    terminal.addEventListener('click', listenTerminalAvoidChangeCursor);
-    terminal.addEventListener('keydown', listenTerminalAvoidMoveCursor)
-})
-
-const captureFunction = (inputText) => {
-    const a =  [...inputText];
-    let stringUser = "";
-    let index, program, params;
-
-    a.forEach((char, i) => {
-        if (char === '>')
             index = i + 1;
-    })
-    stringUser = a.slice(index).join('').split(" ");
-    lastCommands.push(stringUser.join(' '))
-    program = stringUser[0];
-    params = stringUser.slice(1);
-
-    if (program === '') {
-        library.setTerminal('');
-    } else
-        //library[program](params);
-        try {
-            library[program](params);
-        } catch {
-            library.setTerminal(`terminal: ${program} program not found.`);
-        }
-    
+        })
+        stringUser = a.slice(index).join('').split(" ");
+        lastCommands.push(stringUser.join(' '))
+        program = stringUser[0];
+        params = stringUser.slice(1);
+        
+        if (program === '')
+            setTerminal('');
+        else
+        library[program](params);
+        // try {
+        //     library[program](params);
+        // } catch {
+        //     setTerminal(`terminal: ${program} program not found.`);
+        // }
+        
+    }
 }
+
+initTerminal();
